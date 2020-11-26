ea7fb5931ba5c30157a5e4f3bef186afeef76cbd
sho171 <sh.zxcv00@gmail.com>
2020-11-26 21:12:30 +0900

    Modify: connection error対応 No image追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -11,7 +11,7 @@ class Property < ApplicationRecord
   has_one_attached :image
 
   def self.fetch_tokyo_property
-    url = 'https://www.fudousan.or.jp/property/rent/13/area/list?m_adr%5B%5D=13101&m_adr%5B%5D=13102&m_adr%5B%5D=13103&m_adr%5B%5D=13104&m_adr%5B%5D=13105&m_adr%5B%5D=13106&m_adr%5B%5D=13107&m_adr%5B%5D=13108&m_adr%5B%5D=13109&m_adr%5B%5D=13110&m_adr%5B%5D=13111&m_adr%5B%5D=13112&m_adr%5B%5D=13113&m_adr%5B%5D=13114&m_adr%5B%5D=13115&m_adr%5B%5D=13116&m_adr%5B%5D=13117&m_adr%5B%5D=13118&m_adr%5B%5D=13119&m_adr%5B%5D=13120&m_adr%5B%5D=13121&m_adr%5B%5D=13122&m_adr%5B%5D=13123&m_adr%5B%5D=13204&m_adr%5B%5D=13208&m_adr%5B%5D=13219&m_adr%5B%5D=13223&ptm%5B%5D=0303&price_r_from=&price_r_to=130000&keyword=&eki_walk=10&bus_walk=&exclusive_area_from=&exclusive_area_to=&exclusive_area_from=&exclusive_area_to=&floor_plan%5B%5D=2XXXSDK&floor_plan%5B%5D=2XXSLDK&floor_plan%5B%5D=3XXXXSK&floor_plan%5B%5D=3XXXSDK&floor_plan%5B%5D=3XXSLDK&floor_plan%5B%5D=4XXXXSK&floor_plan%5B%5D=4XXXSDK&floor_plan%5B%5D=4ZZZZZZ&built=10'
+    url = 'https://www.fudousan.or.jp/property/rent/13/area/list?prop-control=&sort1=ASRT11&sort2=&limit=100&ptm%5B%5D=0303&price_r_from=&price_r_to=130000&keyword=&eki_walk=10&bus_walk=&exclusive_area_from=&exclusive_area_to=&exclusive_area_from=&exclusive_area_to=&floor_plan%5B%5D=2XXXSDK&floor_plan%5B%5D=2XXSLDK&floor_plan%5B%5D=3XXXXSK&floor_plan%5B%5D=3XXXSDK&floor_plan%5B%5D=3XXSLDK&floor_plan%5B%5D=4XXXXSK&floor_plan%5B%5D=4XXXSDK&floor_plan%5B%5D=4ZZZZZZ&built=10&m_adr%5B%5D=13101&m_adr%5B%5D=13102&m_adr%5B%5D=13103&m_adr%5B%5D=13104&m_adr%5B%5D=13105&m_adr%5B%5D=13106&m_adr%5B%5D=13107&m_adr%5B%5D=13108&m_adr%5B%5D=13109&m_adr%5B%5D=13110&m_adr%5B%5D=13111&m_adr%5B%5D=13112&m_adr%5B%5D=13113&m_adr%5B%5D=13114&m_adr%5B%5D=13115&m_adr%5B%5D=13116&m_adr%5B%5D=13117&m_adr%5B%5D=13118&m_adr%5B%5D=13119&m_adr%5B%5D=13120&m_adr%5B%5D=13121&m_adr%5B%5D=13122&m_adr%5B%5D=13123&m_adr%5B%5D=13204&m_adr%5B%5D=13208&m_adr%5B%5D=13219&m_adr%5B%5D=13223'
 
     agent = Mechanize.new
     page = agent.get(url)
@@ -27,23 +27,29 @@ class Property < ApplicationRecord
       deposit = el.at('.list-info li:nth-child(6)').text.strip.gsub(/\s+/, ' ')
       management = el.at('.list-info li:nth-child(7)').text.strip.gsub(/\s+/, ' ')
       url = "https://www.fudousan.or.jp" + el.at('.prop-title-link').get_attribute(:href)
-      image = el.at('img')['data-echo'] + ".jpg"
-      io = URI.open(image)
+      image = el.at('img')['data-echo'] &.+ ".jpg"
+      if image.nil? || image.include?("gif")
+        image = nil
+      else
+        io = URI.open(image)
+      end
 
       record = self.find_or_initialize_by(title: title, access: access, price: price, floor: floor, area: area, stair: stair, deposit: deposit, management: management, url: url)
       if record.new_record?
         record.save!
-        record.image.attach(io: io, filename: "image-#{record.id}.jpg")
+        record.image.attach(io: io, filename: "image-#{record.id}.jpg") if image.present?
+        io.close if image.present?
         new_properties << record
       else
         logger.info "[INFO] Already saved."
       end
+      sleep 3
     end
     PropertyTokyoMailer.notification(new_properties).deliver_now if new_properties.present?
   end
 
   def self.fetch_kanagawa_property
-    url = 'https://www.fudousan.or.jp/property/rent/14/area/list?m_adr%5B%5D=14101&m_adr%5B%5D=14102&m_adr%5B%5D=14103&m_adr%5B%5D=14104&m_adr%5B%5D=14105&m_adr%5B%5D=14106&m_adr%5B%5D=14107&m_adr%5B%5D=14108&m_adr%5B%5D=14109&m_adr%5B%5D=14110&m_adr%5B%5D=14111&m_adr%5B%5D=14112&m_adr%5B%5D=14113&m_adr%5B%5D=14114&m_adr%5B%5D=14115&m_adr%5B%5D=14116&m_adr%5B%5D=14117&m_adr%5B%5D=14118&m_adr%5B%5D=14131&m_adr%5B%5D=14132&m_adr%5B%5D=14133&m_adr%5B%5D=14134&m_adr%5B%5D=14135&m_adr%5B%5D=14136&m_adr%5B%5D=14137&ptm%5B%5D=0303&price_r_from=&price_r_to=130000&keyword=&eki_walk=10&bus_walk=&exclusive_area_from=&exclusive_area_to=&exclusive_area_from=&exclusive_area_to=&floor_plan%5B%5D=2XXXSDK&floor_plan%5B%5D=2XXSLDK&floor_plan%5B%5D=3XXXXSK&floor_plan%5B%5D=3XXXSDK&floor_plan%5B%5D=3XXSLDK&floor_plan%5B%5D=4XXXXSK&floor_plan%5B%5D=4XXXSDK&floor_plan%5B%5D=4ZZZZZZ&built=10'
+    url = 'https://www.fudousan.or.jp/property/rent/14/area/list?prop-control=&sort1=ASRT11&sort2=&limit=100&ptm%5B%5D=0303&price_r_from=&price_r_to=130000&keyword=&eki_walk=10&bus_walk=&exclusive_area_from=&exclusive_area_to=&exclusive_area_from=&exclusive_area_to=&floor_plan%5B%5D=2XXXSDK&floor_plan%5B%5D=2XXSLDK&floor_plan%5B%5D=3XXXXSK&floor_plan%5B%5D=3XXXSDK&floor_plan%5B%5D=3XXSLDK&floor_plan%5B%5D=4XXXXSK&floor_plan%5B%5D=4XXXSDK&floor_plan%5B%5D=4ZZZZZZ&built=10&m_adr%5B%5D=14101&m_adr%5B%5D=14102&m_adr%5B%5D=14103&m_adr%5B%5D=14104&m_adr%5B%5D=14105&m_adr%5B%5D=14106&m_adr%5B%5D=14107&m_adr%5B%5D=14108&m_adr%5B%5D=14109&m_adr%5B%5D=14110&m_adr%5B%5D=14111&m_adr%5B%5D=14112&m_adr%5B%5D=14113&m_adr%5B%5D=14114&m_adr%5B%5D=14115&m_adr%5B%5D=14116&m_adr%5B%5D=14117&m_adr%5B%5D=14118&m_adr%5B%5D=14131&m_adr%5B%5D=14132&m_adr%5B%5D=14133&m_adr%5B%5D=14134&m_adr%5B%5D=14135&m_adr%5B%5D=14136&m_adr%5B%5D=14137'
 
     agent = Mechanize.new
     page = agent.get(url)
@@ -59,17 +65,23 @@ class Property < ApplicationRecord
       deposit = el.at('.list-info li:nth-child(6)').text.strip.gsub(/\s+/, ' ')
       management = el.at('.list-info li:nth-child(7)').text.strip.gsub(/\s+/, ' ')
       url = "https://www.fudousan.or.jp" + el.at('.prop-title-link').get_attribute(:href)
-      image = el.at('img')['data-echo'] + ".jpg"
-      io = URI.open(image)
+      image = el.at('img')['data-echo'] &.+ ".jpg"
+      if image.nil? || image.include?("gif")
+        image = nil
+      else
+        io = URI.open(image)
+      end
 
       record = self.find_or_initialize_by(title: title, access: access, price: price, floor: floor, area: area, stair: stair, deposit: deposit, management: management, url: url)
       if record.new_record?
         record.save!
-        record.image.attach(io: io, filename: "image-#{record.id}.jpg")
+        record.image.attach(io: io, filename: "image-#{record.id}.jpg") if image.present?
+        io.close if image.present?
         new_properties << record
       else
         logger.info "[INFO] Already saved."
       end
+      sleep 3
     end
     PropertyKanagawaMailer.notification(new_properties).deliver_now if new_properties.present?
   end
