3f9274f9390de340f0d797cb959d2ad02da058b6
yshici <lo.ol.000.yt@gmail.com>
2021-01-25 03:03:19 +0900

    Add: メール設定など

--- app/mailers/property_tokyo_mailer.rb
+++ app/mailers/property_tokyo_mailer.rb
@@ -2,8 +2,8 @@ class PropertyTokyoMailer < ApplicationMailer
   def notification(properties)
     @properties = properties
     subject = '【新着】東京の新着物件'
-    to = Rails.application.credentials.mail[:mail1], Rails.application.credentials.mail[:mail2]
-    from = Rails.application.credentials.mail[:mail3]
+    to = Rails.application.credentials.mail
+    from = Rails.application.credentials.mail
     mail(to: to, from: from, subject: subject)
   end
 end
